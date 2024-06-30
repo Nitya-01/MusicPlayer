64d890666a865d13ff03ab44c68245718b8b5438
NityaGupta01 <153364406+Nitya-01@users.noreply.github.com>
2024-06-30 22:57:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,150 @@
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("controlIcon");
+const playPauseButton = document.querySelector(".play-pause-btn");
+const forwardButton = document.querySelector(".controls button.forward");
+const backwardButton = document.querySelector(".controls button.backward");
+const songName = document.querySelector(".music-player h1");
+const artistName = document.querySelector(".music-player p");
+const volumeControl = document.getElementById("volume");
+
+const songs = [
+  
+  {
+    title: "Please Please Please",
+    name: "Sabrina Carpenter",
+    source: "songs/song02.mp3",
+  },
+  {
+    title: "Dover Beach",
+    name: "Baby Queen",
+    source: "songs/song03.mp3",
+  },
+  {
+    title: "Butterfly Effect",
+    name: "Sophie Holohan",
+    source: "songs/song04.mp3",
+  },
+  {
+    title: "Who Knows",
+    name: "Natasha Bedingfield",
+    source: "songs/song01.mp3",
+  },
+  {
+    title: "Sunset",
+    name: "The xx",
+    source: "songs/song05.mp3",
+  },
+  {
+    title: "Zanzibar",
+    name: "Kamaliza",
+    source: "songs/song06.mp3",
+  },
+  {
+    title: "Softcore",
+    name: "The Neighbourhood",
+    source: "songs/song07.mp3",
+  },
+];
+
+let currentSongIndex = 3;
+
+function updateSongInfo() {
+  songName.textContent = songs[currentSongIndex].title;
+  artistName.textContent = songs[currentSongIndex].name;
+  song.src = songs[currentSongIndex].source;
+
+  song.addEventListener("loadeddata", function () {});
+}
+
+song.addEventListener("timeupdate", function () {
+  if (!song.paused) {
+    progress.value = song.currentTime;
+  }
+});
+
+song.addEventListener("loadedmetadata", function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+});
+
+function pauseSong() {
+  song.pause();
+  controlIcon.classList.remove("fa-pause");
+  controlIcon.classList.add("fa-play");
+}
+
+function playSong() {
+  song.play();
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+}
+
+function playPause() {
+  if (song.paused) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+}
+
+playPauseButton.addEventListener("click", playPause);
+
+progress.addEventListener("input", function () {
+  song.currentTime = progress.value;
+});
+
+progress.addEventListener("change", function () {
+  playSong();
+});
+
+volumeControl.addEventListener("input", function () {
+  const volumeValue = this.value / 100;
+  song.volume = volumeValue;
+  updateVolumeIcon(volumeValue);
+});
+
+function updateVolumeIcon(volume) {
+  const volumeIcon = document.querySelector(".volume-control i");
+  if (volume === 0) {
+    volumeIcon.classList.remove("fa-volume-up");
+    volumeIcon.classList.add("fa-volume-mute");
+  } else {
+    volumeIcon.classList.remove("fa-volume-mute");
+    volumeIcon.classList.add("fa-volume-up");
+  }
+}
+
+forwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+backwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+updateSongInfo();
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  centeredSlides: true,
+  initialSlide: 3,
+  slidesPerView: "auto",
+  allowTouchMove: false,
+  spaceBetween: 40,
+  coverflowEffect: {
+    rotate: 25,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".forward",
+    prevEl: ".backward",
+  },
+});
